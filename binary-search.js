1f2d520df6608f8f0ba3fec6896eba49aa88109d
unknown <Emmanuel Ayodele>
2022-01-13 13:05:12 +0100

    Linear search

---
+++ binary-search.js
@@ -0,0 +1,4 @@
+const binarySearch = (array, value) => {
+    lowerIndex = 0;
+    upperIndex = array.length - 1;
+}
---
+++ linear-search.js
@@ -0,0 +1,23 @@
+const inputArray = [11, 5, 15, 25, 88]
+
+const linearSearch = (array, value) => {
+    //First sort the array, since linear search works with ordered arrays;
+    array.sort((a, b) => a - b)
+
+    //Iterate through array elements;
+    for (let index = 0; index < array.length; index++) {
+        //Return the elemet if the value is found;
+        if (array[index] == value) {
+            return value;
+        }
+        //exit the loop if we reach an element than greater than the value;
+        else if (array[index] > value) {
+            break;
+        }
+    }
+
+    //Return the array if the value is not found.
+    return inputArray;
+};
+
+console.log(linearSearch(inputArray, 75))
